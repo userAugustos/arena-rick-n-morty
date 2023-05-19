a3d638b1de745b6f7e5161253f5da6487c6a92e1
Felipe Augustos <felipe_augustos84@outlook.com.br>
2023-05-19 04:33:50 +0000

    Update 4 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,109 @@
-import './styles/App.css'
-import {useEffect, useState} from "react";
+import "./styles/App.css";
+import { useEffect, useState } from "react";
 
-import { debounce } from "lodash"
-import {useQuery} from "urql";
-import { getCharacters  } from "./queries.ts";
+import { debounce } from "lodash";
+import { useQuery } from "urql";
+import { getCharacters } from "./queries.ts";
 import { variablesSchema } from "./types/variables.types.ts";
-import {Card} from "./components/card.tsx";
+import { Card } from "./components/card.tsx";
 import { character } from "./types/character.types.ts";
-import {Loading} from "./components/loading.tsx";
+import { Loading } from "./components/loading.tsx";
 
 function App() {
-    const [variables, setVariables] = useState<variablesSchema>({
-        page: 1,
-        filter: {}
+  const [variables, setVariables] = useState<variablesSchema>({
+    page: 1,
+    filter: {},
+  });
+
+  const [{ data, fetching, error }] = useQuery({
+    query: getCharacters,
+    variables,
+  });
+  const handleSearchCharacter = debounce((event: HTMLInputElement) => {
+    setVariables({
+      ...variables,
+      filter: {
+        ...variables.filter,
+        name: event.value,
+      },
     });
+  }, 1000);
 
-    const [{ data, fetching, error }] = useQuery({
-        query: getCharacters,
-        variables
-    });
-    const handleSearchCharacter = debounce((event: HTMLInputElement) => {
-        setVariables({
-            ...variables,
-            filter: {
-                ...variables.filter,
-                name: event.value
-            }
-        })
-    }, 1000)
-
-    useEffect(() => {
-        document.querySelector('#root')?.scrollIntoView({
-            behavior: 'smooth'
-        })
-    }, [data, variables])
-
-    const handleSearchByStatus = (e:  React.ChangeEvent<HTMLSelectElement>) => {
-        if(e.target.value === 'default') {
-            return
-        }
-        setVariables({
-            ...variables,
-            filter: {
-                ...variables.filter,
-                status: e.target.value
-            }
-        })
+  const handleSearchByStatus = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === "default") {
+      return;
     }
+    setVariables({
+      ...variables,
+      filter: {
+        ...variables.filter,
+        status: e.target.value,
+      },
+    });
+  };
 
-    return (
-        <div className="container flex">
-            <div className="hero center-flex">
-                <h1>The Rick 'n Morty Wiki</h1>
-                <img src="/arena-rick-n-morty/rick-n-morty.png" alt=""/>
-            </div>
-            <div className="content flex">
-                <section className="inputs flex">
-                    <input
-                        type="text"
-                        name="character"
-                        id="character"
-                        onKeyUp={(e) => handleSearchCharacter(e.currentTarget)}
-                        placeholder="Search"
-                    />
-                    <select name="status" id="status" onChange={handleSearchByStatus}>
-                        <option value="default" className="blur">Status</option>
-                        <option value="Alive">Alive</option>
-                        <option value="Dead">Dead</option>
-                        <option value="unknown">unknown</option>
-                    </select>
-                </section>
-                { fetching && <Loading /> }
-                { error && <p>{error?.message}</p> }
-                { (!fetching && data?.characters) && (
-                        <>
-                        <ul className="list flex">
-                            {data?.characters?.results?.map((character) => (
-                                <Card character={character as character} />
-                            ))}
-                        </ul>
-                        <section className="prev-next flex">
-                            <button onClick={()=> setVariables({...variables, page: variables.page - 1})}>{'<'}</button>
-                            <button onClick={() => setVariables({...variables, page: variables.page + 1})}>{'>'}</button>
-                        </section>
-                        </>
-                    )
+  useEffect(() => {
+    document.querySelector("#root")?.scrollIntoView({
+      behavior: "smooth",
+    });
+  }, [data, variables]);
+
+  return (
+    <div className="container flex">
+      <div className="hero center-flex">
+        <h1>The Rick 'n Morty Wiki</h1>
+        <img src="/arena-rick-n-morty/rick-n-morty.png" alt="" />
+      </div>
+      <div className="content flex">
+        <section className="inputs flex">
+          <input
+            type="text"
+            name="character"
+            id="character"
+            onKeyUp={(e) => handleSearchCharacter(e.currentTarget)}
+            placeholder="Search"
+          />
+          <select name="status" id="status" onChange={handleSearchByStatus}>
+            <option value="default" className="blur">
+              Status
+            </option>
+            <option value="Alive">Alive</option>
+            <option value="Dead">Dead</option>
+            <option value="unknown">unknown</option>
+          </select>
+        </section>
+        {fetching && <Loading />}
+        {error && <p>{error?.message}</p>}
+        {!fetching && data?.characters && (
+          <>
+            <ul className="list flex">
+              {data?.characters?.results?.map((character: character) => (
+                <li key={character.id}>
+                  <Card character={character} />
+                </li>
+              ))}
+            </ul>
+            <section className="prev-next flex">
+              <button
+                onClick={() =>
+                  setVariables({ ...variables, page: variables.page - 1 })
                 }
-            </div>
-        </div>
-    )
+              >
+                {"<"}
+              </button>
+              <button
+                onClick={() =>
+                  setVariables({ ...variables, page: variables.page + 1 })
+                }
+              >
+                {">"}
+              </button>
+            </section>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,27 +1,27 @@
 import { character } from "../types/character.types.ts";
-import '../styles/card.css'
-export function Card({ character }: { character: character}) {
-    return (
-        <li key={character.id} className="card flex">
-            <img src={character.image} alt="personagem"/>
-            <section className="info flex">
-                {/*in case we doing redirect in future*/}
-                <section key={character.id}>
-                    <h2>{character.name}</h2>
-                    <span className={`status_icon ${character.status}`} ></span>
-                    <span>{character.status}</span>
-                </section>
-                {/* key={character.location?.id} */}
-                <section>
-                    <span>Ultima vez visto em:</span>
-                    <p>{character.location?.name}</p>
-                </section>
-                {/* key={character.episode[0].id} */}
-                <section>
-                    <span>Primeira aparição:</span>
-                    <p>{character.episode[0].name}</p>
-                </section>
-            </section>
-        </li>
-    )
+import "../styles/card.css";
+export function Card({ character }: { character: character }) {
+  return (
+    <div className="card flex">
+      <img src={character.image} alt="personagem" />
+      <section className="info flex">
+        {/*in case we doing redirect in future*/}
+        <section key={character.id}>
+          <h2>{character.name}</h2>
+          <span className={`status_icon ${character.status}`}></span>
+          <span>{character.status}</span>
+        </section>
+        {/* key={character.location?.id} */}
+        <section>
+          <span>Ultima vez visto em:</span>
+          <p>{character.location?.name}</p>
+        </section>
+        {/* key={character.episode[0].id} */}
+        <section>
+          <span>Primeira aparição:</span>
+          <p>{character.episode[0].name}</p>
+        </section>
+      </section>
+    </div>
+  );
 }
