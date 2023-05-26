19d39cdabb2f0ffaebde37ac74776b8ff30935f3
userAugustos <felipe_augustos84@outlook.com.br>
2023-05-26 13:58:01 -0300

    feat: making the dialog with showModal work perfect

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,38 +1,38 @@
-import { character } from "../types/character.types.ts";
+import {character} from "../types/character.types.ts";
 import {Link, useLocation} from "react-router-dom";
 import "../styles/card.css";
 
 const base = import.meta.env.VITE_BASE_PATH
 
-export function Card({ character }: { character: character }) {
-  const location = useLocation();
+export function Card({character}: { character: character }) {
+    const location = useLocation();
 
-  return (
-      <Link
-          className="card flex"
-          key={character.id}
-          to={`${base}/character/${character.id}`}
-          state={{ backgroundLocation: location }}
-      >
-      <img src={character.image} alt="personagem" />
-      <section className="info flex">
-        {/*in case we doing redirect in future*/}
-        <section key={character.id}>
-          <h2>{character.name}</h2>
-          <span className={`status_icon ${character.status}`}></span>
-          <span>{character.status}</span>
-        </section>
-        {/* key={character.location?.id} */}
-        <section>
-          <span>Ultima vez visto em:</span>
-          <p>{character.location?.name}</p>
-        </section>
-        {/* key={character.episode[0].id} */}
-        <section>
-          <span>Primeira aparição:</span>
-          <p>{character.episode[0].name}</p>
-        </section>
-      </section>
-      </Link>
-  );
+    return (
+        <Link
+            className="card flex"
+            key={character.id}
+            to={`${base}/character/${character.id}`}
+            state={{backgroundLocation: location}}
+        >
+            <img src={character.image} alt="personagem"/>
+            <section className="info flex">
+                {/*in case we doing redirect in future*/}
+                <section>
+                    <h2>{character.name}</h2>
+                    <span className={`status_icon ${character.status}`}></span>
+                    <span>{character.status}</span>
+                </section>
+                {/* key={character.location?.id} */}
+                <section>
+                    <span>Ultima vez visto em:</span>
+                    <p>{character.location?.name}</p>
+                </section>
+                {/* key={character.episode[0].id} */}
+                <section>
+                    <span>Primeira aparição:</span>
+                    <p>{character.episode[0].name}</p>
+                </section>
+            </section>
+        </Link>
+    );
 }
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,7 +1,37 @@
-export default function Modal(){
-    return(
-        <div>
+import {useCallback, useEffect, useRef, useState} from "react";
+import {useLocation} from "react-router-dom";
+
+export default function Modal() {
+    const location = useLocation()
+    const {pathname: path} = location
+    // const [isOpen, setIsOpen] = useState(true);
+
+    const dialogRef = useRef<HTMLDialogElement>(null)
+    const handleCloseModal = () => {
+        dialogRef.current?.close()
+        // setIsOpen(false)
+    }
+
+    // close the modal on off modal click
+    const onClick = useCallback(({target}: any) => {
+        const {current: el} = dialogRef;
+        if (target === el) handleCloseModal()
+    }, [])
+
+    useEffect(() => {
+        const dialog = dialogRef.current;
+        dialog?.showModal()
+        // reset the modal effect
+        console.debug(path)
+        return () => dialog?.close()
+    }, [path])
+
+    return (
+        <dialog
+            ref={dialogRef}
+            onClick={onClick}
+        >
             hello
-        </div>
+        </dialog>
     )
 }
